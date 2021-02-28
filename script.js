0609d3a12cd5d9627fdee497dfbbef018936483c
Arroxa <vitor.league@gmail.com>
2021-02-28 16:34:44 -0300

    Atualização do Grid e FlexBox

--- script.js
+++ script.js
@@ -2,3 +2,10 @@ window.addEventListener('scroll', () => {
     var header = document.querySelector('header');
     header.classList.toggle('sticky', window.scrollY > 0);
 })
+
+const questionCard = document.querySelectorAll('.question-card');
+const colorHex = document.querySelector('.question-card img');
+console.log(colorHex);
+
+
+Array.prototype.map.call(questionCard, card => card.style.boxShadow = '');
