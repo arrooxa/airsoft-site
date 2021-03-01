8bd90eb65e9fcb41fc1c00108665b5bfd48091be
Arroxa <vitor.league@gmail.com>
2021-02-28 23:16:31 -0300

    Adição de funcionalidades novas

---
+++ data.js
@@ -0,0 +1,9 @@
+const data = 
+[
+    {
+        nome: "Vitor Rocha Cambuí",
+        nascimento: 27/08/2001,
+        apelido: "Arroxa",
+        cor: "Orquídea-Média"
+    }
+];
--- script.js
+++ script.js
@@ -1,11 +1,32 @@
+const questionCard = document.querySelectorAll('.question-card');
+const questionImage = document.querySelectorAll('.question-card img')[1];
+const colorHex = questionImage.src.substring(questionImage.src.indexOf('colors/', 0) + 7, questionImage.src.length - 4);
+const profileImage = document.querySelector('.profile-image');
+const closeIcon = document.querySelector('.close-icon');
+const page = document.querySelector('.page');
+const imageFocus = document.querySelector('.click-function');
+const dataAirsoft = require('data.js');
+
+console.log(dataAirsoft);
+
+
 window.addEventListener('scroll', () => {
     var header = document.querySelector('header');
     header.classList.toggle('sticky', window.scrollY > 0);
 })
 
-const questionCard = document.querySelectorAll('.question-card');
-const colorHex = document.querySelector('.question-card img');
-console.log(colorHex);
+profileImage.addEventListener('click', () => {
+    imageFocus.style.display = 'block';
+    page.style.opacity = '0.1';
+    closeIcon.style.cursor = 'pointer';
+    document.querySelector('*').style.overflow = 'hidden'; 
+        closeIcon.addEventListener('click', () => {
+            imageFocus.style.display = 'none';
+            page.style.opacity = '1';
+            document.querySelector('*').style.overflow = 'auto';
+        })
+})
 
-
-Array.prototype.map.call(questionCard, card => card.style.boxShadow = '');
+Array.prototype.map.call(questionCard, card => {
+    card.style.boxShadow = `3px 3px 2px #${colorHex}`
+});
